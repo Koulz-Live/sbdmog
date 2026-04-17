3b34d1f38958ad713abe66fff5d8552b06bc1b2e
Khulekani-22 <khulekani@22onsloane.co>
2026-04-17 20:43:00 +0200

    fix: install bootstrap-icons package and import CSS in main.tsx

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 import { App } from './App.js';
 import './index.css';
 
